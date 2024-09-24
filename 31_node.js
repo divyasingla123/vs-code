e06d1a2909c93e15eae30e682f663b55b3aa7a63
divyasingla123 <divyasingla05@gmail.com>
2024-09-24 19:21:05 +0530

    .

--- 31_node.js
+++ 31_node.js
@@ -3,3 +3,10 @@
 // node.js comes with REPL[read-eval-]
 // ctrl+L to clear command
 //prompt-sync module to take user input
+//  node.js se hum js ka code command prompt pe run hoga
+// node.js se hum server side bhi run kr skte h 
+//  npx or npm modules install krne k liye use hota h 
+//  zyada bde code node shell me ni de skte
+// console.clear ya fr ctrl + l use hota h sb clear krne ne k liye 
+// ... tb ata h jb hume multiline code dena hota h 
+// .break continuity break krne k liye use hota h  
